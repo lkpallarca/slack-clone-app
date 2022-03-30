f9368068e8ea9f45979224ab683b719a26ed206e
lkpallarca <kencepallarca@gmail.com>
2022-03-30 21:33:49 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './css/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Consilium ac implerem
+      </header>
+    </div>
+  );
+}
+
+export default App;
