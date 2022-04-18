79c058d2aa49f66cc38bf0e1772f5c815ed47a65
lkpallarca <kencepallarca@gmail.com>
2022-04-18 11:33:58 +0800

    Add scroll to last message

--- src/components/main-display/sub-components/conversation-screen/sub-components/ConversationDisplay.js
+++ src/components/main-display/sub-components/conversation-screen/sub-components/ConversationDisplay.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import API from '../../../../../API';
 import { getLoggedUser } from '../../../../../utils/storage';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function ConversationDisplay({ messages }) {
+  const setRef = useCallback(node => {
+    if(node) {
+      node.scrollIntoView({smooth : true})
+    }
+  }, [])
+
   return (
     <section className='conversation-display'>
-      {messages.map(each => {
+      {messages.map((each, index) => {
+        const lastMessage = messages.length - 1 === index;
         if(each.sender.id === getLoggedUser().data.id) {
           return (
-            <div key={uuidv4()} className='message-bubble me'> 
+            <div key={index} ref={lastMessage ? setRef : null} className='message-bubble me'> 
               {each.body}
             </div>
           )
         } else {
           return (
-            <div key={uuidv4()} className='message-bubble'>
+            <div key={index} ref={lastMessage ? setRef : null} className='message-bubble'>
               {each.body}
             </div>
           )
